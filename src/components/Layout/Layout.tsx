8e9fb5688344b3239167eaa603265d7470381596
Kirill Dyatel <kirill.dyatel.03@mail.ru>
2026-06-11 14:22:07 +0300

    add login Modal

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
+import { AuthModal } from "../Auth/AuthModal";
 
 export const Layout = () => {
   return (
@@ -10,6 +11,7 @@ export const Layout = () => {
         <Outlet />
       </main>
       <Footer />
+      <AuthModal />
     </div>
   );
 };
